e3f0ab638ebe2d1b17eefd1f383b5506e3db6622
Phillip Horn <23446987+BeardedPhill@users.noreply.github.com>
2019-07-24 15:02:01 +0200

    Add some logic to handle messages that was put on a queue from sns. (#4)

--- message.py
+++ message.py
@@ -1,11 +1,21 @@
 import time
+import json
 
 class Message():
     def __init__(self, message=None):
         timestamp = float(message['Attributes']['SentTimestamp']) / 1000
+        message_body = json.loads(message['Body'])
+
         self.id = message['MessageId']
         self.timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
-        self.body = message['Body']
+        try:
+            if message_body['Type']=='Notification':
+                self.body = message_body['Message']
+            else:
+                self.body = json.dumps(message_body)
+        except KeyError:
+            self.body = json.dumps(message_body)
+
 
 class SqsFormData():
     def __init__(self, queue_url=None, max_messages=None, delete_messages=None):
